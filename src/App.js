a632a46bde18df01b2117eab80f9609f9a216886
Nojus R <42602537+had0pelagic@users.noreply.github.com>
2022-06-20 20:42:05 +0300

    Axios update, UI update, colored viewer border

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import api from "./api";
 import Loader from "./components/loading_spinner";
 import styled from "styled-components";
 import ModelViewer from "./components/model_viewer";
-import CardBackside from "./files/images/card_backside.png";
+import { CardRarities } from "./util/constants/card_rarities";
 import Dark from "./files/images/DARK.png";
 import Divine from "./files/images/DIVINE.png";
 import Earth from "./files/images/EARTH.png";
@@ -40,6 +39,7 @@ export default function App() {
     WATER: Water,
     WIND: Wind,
   };
+  const [maxRarity, setMaxRarity] = useState({});
 
   useEffect(() => {
     getRandomCard();
@@ -50,8 +50,9 @@ export default function App() {
 
     if (response.status === 200) {
       setCard(response.data);
+      getMaxRarity(response.data);
     } else {
-      console.log("error");
+      alert(response.data);
     }
 
     setLoading(false);
@@ -63,13 +64,42 @@ export default function App() {
     if (response.status === 200) {
       setCards(response.data.data);
     } else {
-      console.log("error");
+      alert(response.data);
     }
 
     setLoading(false);
   };
 
+  const getMaxRarity = (data) => {
+    if (data.hasOwnProperty("card_sets") && data.card_sets.length > 0) {
+      var foundRarities = [];
+
+      CardRarities.forEach((rarity) => {
+        data.card_sets.forEach((set) => {
+          if (
+            rarity.name === set.set_rarity.toUpperCase() &&
+            rarity.rank !== -1
+          ) {
+            return foundRarities.push(rarity);
+          }
+        });
+      });
+
+      var topRarity = foundRarities
+        .filter((item, index) => {
+          return foundRarities.indexOf(item) === index;
+        })
+        .sort((a, b) => (a.rank > b.rank ? 1 : -1));
+
+      console.log(topRarity[0].name);
+      setMaxRarity(topRarity[0]);
+    } else {
+      setMaxRarity(CardRarities[0]);
+    }
+  };
+
   const handleSearchChange = (e) => {
+    e.preventDefault();
     const { id, value } = e.target;
     setSearchForm((prevState) => ({
       ...prevState,
@@ -83,22 +113,156 @@ export default function App() {
 
   const handleSelectChange = (e) => {
     setCard(cards[e.target.value]);
+    getMaxRarity(cards[e.target.value]);
   };
 
-  const CardDescriptionContainer = (
-    <div className="description_container">
-      {card.hasOwnProperty("desc") ? (
-        <>
-          <p className="description_text">{card.desc}</p>
-        </>
-      ) : (
-        <></>
-      )}
+  return (
+    <div className="App">
+      <header className="App-header">
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="main">
+            <div className="main_grid">
+              {CardDescriptionContainer(card)}
+              {CenterContainer(maxRarity, card)}
+              {CardInformationContainer(card, effect_cards, attribute_images)}
+            </div>
+            <div className="action_container">
+              {ActionContainer(
+                getRandomCard,
+                handleSearchSubmit,
+                handleSearchChange,
+                getCardByName,
+                handleSelectChange,
+                searchForm,
+                cards
+              )}
+            </div>
+          </div>
+        )}
+      </header>
     </div>
   );
+}
 
-  const CardInformationContainer = (
-    <>
+const CenterGridContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin: 20px 20px 20px 20px;
+  width: 460px;
+  height: 460px;
+  background: black;
+  overflow: hidden;
+  border-radius: 20px;
+
+  :before {
+    position: absolute;
+    content: "";
+    width: 800px;
+    height: 140%;
+    background: linear-gradient(
+      ${(props) => props.rarity.color1},
+      ${(props) => props.rarity.color2},
+      ${(props) => props.rarity.color3},
+      ${(props) => props.rarity.color4},
+      ${(props) => props.rarity.color5}
+    );
+    animation: moveGrid 2s linear infinite;
+  }
+
+  :after {
+    position: absolute;
+    content: "";
+    background: black;
+    inset: 4px;
+    border-radius: 16px;
+  }
+`;
+
+function CardDescriptionContainer(card) {
+  return (
+    <div className="grid_container" id="left_container">
+      <div className="description_container">
+        {card.hasOwnProperty("desc") ? (
+          <>
+            <p className="description_text">{card.desc}</p>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function CenterContainer(maxRarity, card) {
+  return (
+    <CenterGridContainer rarity={maxRarity}>
+      <div className="viewer_container">
+        <ModelViewer image={card.card_images[0].image_url} />
+      </div>
+    </CenterGridContainer>
+  );
+}
+
+function ActionContainer(
+  getRandomCard,
+  handleSearchSubmit,
+  handleSearchChange,
+  getCardByName,
+  handleSelectChange,
+  searchForm,
+  cards
+) {
+  return (
+    <div className="action_grid">
+      <div className="action_left_container">
+        <button className="action_button" onClick={getRandomCard}>
+          Random card
+        </button>
+      </div>
+
+      <div className="action_center_container">
+        <form className="search_form" onSubmit={handleSearchSubmit}>
+          <input
+            className="search_input"
+            id="name"
+            value={searchForm.name}
+            onChange={handleSearchChange}
+          />
+          <button
+            style={{ marginLeft: "10px" }}
+            className="action_button"
+            onClick={getCardByName}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+
+      <div className="action_right_container">
+        {cards && cards.length > 0 ? (
+          <select className="select" onChange={handleSelectChange}>
+            {cards.map((card, index) => (
+              <option key={index} value={index}>
+                {card.name}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function CardInformationContainer(card, effect_cards, attribute_images) {
+  return (
+    <div className="grid_container" id="right_container">
       {card && !effect_cards.includes(card.type) ? (
         <div className="information_container" id="monster_card">
           <div>{card.name}</div>
@@ -157,78 +321,6 @@ export default function App() {
           )}
         </div>
       )}
-    </>
-  );
-
-  const ActionContainer = (
-    <div className="action_grid">
-      <div className="action_left_container">
-        <button className="action_button" onClick={() => getRandomCard()}>
-          Random card
-        </button>
-      </div>
-
-      <div className="action_center_container">
-        <form className="search_form" onSubmit={handleSearchSubmit}>
-          <input
-            className="search_input"
-            id="name"
-            value={searchForm.name}
-            onChange={handleSearchChange}
-          />
-          <button
-            style={{ marginLeft: "10px" }}
-            className="action_button"
-            onClick={() => getCardByName()}
-          >
-            Search
-          </button>
-        </form>
-      </div>
-
-      <div className="action_right_container">
-        {cards && cards.length > 0 ? (
-          <select className="select" onChange={handleSelectChange}>
-            {cards.map((card, index) => (
-              <option key={index} value={index}>
-                {card.name}
-              </option>
-            ))}
-          </select>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
-  );
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        {loading ? (
-          <Loader />
-        ) : (
-          <div className="main">
-            <div className="main_grid">
-              <div className="grid_container" id="left_container">
-                {CardDescriptionContainer}
-              </div>
-
-              <div className="grid_container" id="center_container">
-                <div className="viewer_container">
-                  <ModelViewer image={card.card_images[0].image_url} />
-                </div>
-              </div>
-
-              <div className="grid_container" id="right_container">
-                {CardInformationContainer}
-              </div>
-            </div>
-
-            <div className="action_container">{ActionContainer}</div>
-          </div>
-        )}
-      </header>
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,25 @@ const instance = axios.create({
   baseURL: "https://db.ygoprodeck.com/api/v7/",
 });
 
+const handleResponse = async (response, statusCode) => {
+  return {
+    status: statusCode,
+    data: response.data,
+    headers: response.headers,
+  };
+};
+
 const api = {
   get: async (path) => {
-    return instance.get(path);
+    return instance
+      .get(path)
+      .then(async function (response) {
+        return handleResponse(response, response.status);
+      })
+      .catch(async function (response) {
+        response.data = response.response.data.error;
+        return handleResponse(response, response.response.status);
+      });
   },
 };
 
